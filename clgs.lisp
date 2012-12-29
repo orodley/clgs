84005da1020b2935be1358466062b222d2101dc4
Owen Rodley <Strigoides@gmail.com>
2012-12-29 17:16:46 +1300

    Execution of golfscript code strings, + function for integers, refactoring

--- clgs.lisp
+++ clgs.lisp
@@ -19,33 +19,38 @@
   stack, without modifiying it."
   (subseq *stack* 0 depth))
 
-;;; Parsing
+(defun set-stack (stack-values)
+  "Remove all current stack values and set stack to the values in STACK-VALUES"
+  (setf *stack* stack-values))
 
-(defun tokenize (gs-code-string)
-  "Return a list of string tokens from golfscript source."
-  (cl-ppcre:all-matches-as-strings
-    ;; Regex taken from golfscript.rb source
-    ;; ---variable name---------{block}-----'string'----------"string"-------integer-----comment--single character token
-    "[a-zA-Z_][a-zA-Z0-9_]*|{(?:\\.|[^'])*}|'(?:\\.|[^'])*'?|\"(?:\\.|[^\"])*\"?|-?[0-9]+|#[^\\n\\r]*|."
-    gs-code-string))
+;;; Variable table & related functions
+(defvar *variable-table* (make-hash-table)
+  "Holds variable definitions, including functions.
+  Indexed by symbols")
 
-(defun read-gs-literal (token-string)
-  "Read a literal string or integer token into a golfscript type.
-  Note that [ and ] do not delimit array literals; they are defined
-  as built-in functions."
-  (case (char token-string 0)
-    (#\' (cl-ppcre:regex-replace-all 
-           "([^\\\\]|^)\\\\" (string-trim "'" token-string) "\\1"))
-    (#\{ (make-gs-block
-           (string-trim "{}" token-string)))
-    ;; TODO: Doesn't handle some escape sequences i.e. \n
-    (#\" (make-gs-string
-           (string-trim "\"" token-string)))
-    ;; Normal Lisp reader will do for integers
-    (otherwise (make-gs-integer 
-                 (read-from-string token-string)))))
+(defvar *builtins* (make-hash-table)
+  "Holds all builtin functions and variables")
 
-;;; Structs used for the 4 types
+(defun add-to-var-table (name value table)
+  "Add VALUE to TABLE under key NAME"
+  (setf (gethash name table) value))
+
+(defun get-from-var-table (name table)
+  "Retrieve the entry in TABLE stored under NAME. Secondary return value
+  indicates whether NAME was found"
+  (gethash name table))
+
+(defun clear-var-table (table)
+  "Removes all values from TABLE"
+  (clrhash table))
+
+(defun reset-var-table ()
+  "Restore variable table to builtins only"
+  (clear-var-table *variable-table*)
+  (loop for var being the hash-keys in *builtins* using (hash-value value) do
+        (add-to-var-table var value *variable-table*)))
+
+;;; Types
 (defstruct (gs-integer
              (:constructor make-gs-integer (value)))
   (value 0   :type integer))
@@ -59,9 +64,10 @@
              (:constructor make-gs-block   (value)))
   (value ""  :type string))
 
-(defvar *variable-table* (make-hash-table)
-  "Holds variable definitions, including functions.
-  Indexed by symbols")
+(defun gs-var-value (gs-var)
+  "Return the value contained in a golfscript variable"
+  (slot-value gs-var 'value))
+
 
 (defun priority (type)
   "Return coercion priority for a type (higher values take precedence)"
@@ -78,6 +84,57 @@
     (2 'gs-array)
     (3 'gs-string)
     (4 'gs-block)))
+
+;;; Parsing
+
+(defun tokenize (gs-code-string)
+  "Return a list of string tokens from golfscript source."
+  (cl-ppcre:all-matches-as-strings
+    ;; TODO: Doesn't tokenize strings with escaped quotes correctly
+    ;; ---variable name---------{block}-----'string'----------"string"-------integer-----comment--single character token
+    "[a-zA-Z_][a-zA-Z0-9_]*|{(?:\\.|[^'])*}|'(?:\\.|[^'])*'?|\"(?:\\.|[^\"])*\"?|-?[0-9]+|#[^\\n\\r]*|."
+    gs-code-string))
+
+(defun read-gs-literal (token-string)
+  "Read a literal string or integer token into a golfscript type.
+  Note that [ and ] do not delimit array literals; they are defined
+  as built-in functions."
+  (case (char token-string 0)
+    (#\' (make-gs-string
+           (cl-ppcre:regex-replace-all 
+             "([^\\\\]|^)\\\\" (string-trim "'" token-string) "\\1")))
+    (#\{ (make-gs-block
+           (string-trim "{}" token-string)))
+    ;; TODO: Doesn't handle some escape sequences i.e. \n
+    (#\" (make-gs-string
+           (string-trim "\"" token-string)))
+    (otherwise (make-gs-integer 
+                 (read-from-string token-string)))))
+
+(defun gs-literal-p (token-string)
+  (declare (type string token-string))
+  (let ((first-char (char token-string 0)))
+    (or (member first-char
+                '(#\' #\" #\{ #\-))
+        (digit-char-p first-char))))
+
+(defun execute-gs-string (gs-code-string &optional stack-values)
+  "Execute GS-CODE-STRING as golfscript code, optionally providing starting stack values.
+  Return value of stack on completion"
+  (set-stack stack-values)
+  (reset-var-table)
+  ()
+  (loop for token in (tokenize gs-code-string) do
+        (cond
+          ((gs-literal-p token)
+           (stack-push (read-gs-literal token)))
+          ;; OPTIMIZATION: Loads of duplication here
+          ((get-from-var-table (intern token) *variable-table*)
+           (funcall (get-from-var-table (intern token) *variable-table*)))
+          ((string-equal token " ")
+           ())
+          (t (error "Unrecognized token ~S" token))))
+  *stack*)
 
 (defun match-arg-type (arg-combinations)
   "Inspect the stack and return the first matching argument combination.
@@ -98,14 +155,14 @@
             (apply #'max
                    (mapcar #'priority args)))))
     (mapcar (lambda (arg)
-              (coerce-gs-type arg highest-priority-type))
+              (coerce-gs-object arg highest-priority-type))
             args)))
 
 (defun coerce-gs-object (object type)
   "Return OBJECT coerced to TYPE. Signal error if invalid coercion"
   (if (eql type (type-of object))
     object
-    (let ((value (slot-value object 'value)))
+    (let ((value (gs-var-value object)))
       (etypecase object
         (gs-integer
           (ecase type
@@ -118,7 +175,7 @@
             ;; TODO: This breaks on nested arrays, e.g. [[51 52][53 54]]"55"+
             ;; and non-integer arrays
             (gs-string (make-gs-string (map 'string (lambda (x)
-                                                      (code-char (slot-value x 'value)))
+                                                      (code-char (gs-var-value x)))
                                             value)))
             (gs-block  (make-gs-block 
                          ;; Coerce all elements of the array to
@@ -135,7 +192,7 @@
             (gs-block (make-gs-block (concatenate 'string value " ")))))))))
 
 (defmacro define-gs-function ((name &optional (coerce-n 0)) &body arg-cases)
-  "Define a new function and insert it into the variable table.
+  "Define a builtin function and insert it into *builtins*.
   Each case defines a different function to perform depending
   on the types of the arguments."
   ;; Check for invalid types in ARG-CASES
@@ -147,36 +204,39 @@
                              gs-block))
         (error "In definition of golfscript function \"~S\": ~S is not a valid golfscript type"
                name type))))
-  `(setf (gethash (quote ,name) *variable-table*)
-         (lambda ()
-           ;; Coerce args if necessary
-           ,(when (plusp coerce-n)
-              `(loop for coerced-arg in 
-                     (reverse (coerce-args
-                                (loop repeat ,coerce-n collecting
-                                      (stack-pop))))
-                     do (stack-push coerced-arg)))
-           (cond
-             ;; Set up cond clauses for each arg type combination
-             ,@(mapcar (lambda (arg-case)
-                         `((equal (mapcar #'type-of
-                                          (stack-peek ,(length (car arg-case))))
-                                  (quote ,(car arg-case)))
-                           ,@(cdr arg-case))) 
-                       arg-cases)
-             ;; Fallen through all possible combinations; invalid function call
-             (t (error "~S called with invalid argument types; didn't match any of expected cases: ~S"
-                       (quote ,name)
-                       (quote ,(mapcar #'car arg-cases))))))))
+  `(add-to-var-table (quote ,name)
+                     (lambda ()
+                       ;; Coerce args if necessary
+                       ,(when (plusp coerce-n)
+                          `(loop for coerced-arg in 
+                                 (reverse (coerce-args
+                                            (loop repeat ,coerce-n collecting
+                                                  (stack-pop))))
+                                 do (stack-push coerced-arg)))
+                       (cond
+                         ;; Set up cond clauses for each arg type combination
+                         ,@(mapcar (lambda (arg-case)
+                                     `((equal (mapcar #'type-of
+                                                      (stack-peek ,(length (car arg-case))))
+                                              (quote ,(car arg-case)))
+                                       ,@(cdr arg-case))) 
+                                   arg-cases)
+                         ;; Fallen through all possible combinations; invalid function call
+                         (t (error "~S called with invalid argument types; didn't match any of expected cases: ~S"
+                                   (quote ,name)
+                                   (quote ,(mapcar #'car arg-cases))))))
+                     *builtins*))
 
 #+nil
 (define-gs-function ~ (a b)
                     )
 
-#+nil
 (define-gs-function (+ 2) 
-                    ;; Test macro
-  ((gs-string) do stuff)
-  ((gs-integer) do other stuff)
-  ((gs-array) do more stuff)
-  ((gs-block) do even more stuff))
+  ((gs-integer)
+   (let ((a (gs-var-value (stack-pop)))
+         (b (gs-var-value (stack-pop))))
+     (stack-push (make-gs-integer
+                   (+ a b)))))
+  ((gs-string) ())
+  ((gs-array) ())
+  ((gs-block) ()))
