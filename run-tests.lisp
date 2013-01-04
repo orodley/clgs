07a1a8234f7af47d6812294de48f2d3a2fcfcbfe
Owen Rodley <Strigoides@gmail.com>
2013-01-04 17:25:54 +1300

    Compare output of running test cases to expected, fix various test cases to match output format of EXECUTE-GS-PROGRAM

--- run-tests.lisp
+++ run-tests.lisp
@@ -1,14 +1,24 @@
 ;;;; Runs all tests from "test-cases.txt"
 (load "clgs.lisp")
 
-(let ((test-cases (with-open-file (file #P"test-cases.lisp")
-                    (read file))))
-  (loop for test-case in test-cases do
+(loop for test-case in (with-open-file (file #P"test-cases.lisp")
+                         (read file)) 
+      do
+      (let ((test-case-output ""))
         (handler-case
           ;; Redirect stdout from EXECUTE-GS-PROGRAM
-          (with-open-stream (*standard-output*
-                              (make-string-output-stream))
-            (execute-gs-program (car test-case))
-            (get-output-stream-string *standard-output*))
+          (progn
+            (with-open-stream (*standard-output*
+                                (make-string-output-stream))
+              (execute-gs-program (car test-case))
+              (setf test-case-output
+                    (get-output-stream-string *standard-output*))) 
+            (unless (string= (cadr test-case)
+                             test-case-output)
+              (format t "Output was different for ~A:~% Expected~%  ~A~% Got~%  ~A~%~%"
+                      (car test-case) (cadr test-case) test-case-output))) 
           (error (error)
-                 (format t "~A raised:~% ~A~%~%" (car test-case) error)))))
+                 (format t "~A raised:~% ~A~%~%" (car test-case) error))
+          (warning (warning)
+                   (format t "~A raised warning:~% ~A~%~%"
+                           (car test-case) warning)))))
